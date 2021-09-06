321d41944b41507316d2b61ca33747d11177d520
Marko Mikulic <mikulic.marko996@gmail.com>
2021-09-06 23:17:39 +0200

    Added common http status codes and messages

---
+++ http-status-code-and-messages.go
@@ -0,0 +1,47 @@
+package mop_common
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+var ErrUnauthorized = errors.New("unauthorized")
+
+func ThrowStatusOK(i interface{}, c *gin.Context) {
+	if i != nil {
+		c.JSON(http.StatusOK, i)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "OK",
+	})
+}
+
+func ThrowStatusCreated(i interface{}, c *gin.Context) {
+	if i != nil {
+		c.JSON(http.StatusCreated, i)
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status":  http.StatusCreated,
+		"message": "OK",
+	})
+}
+
+func ThrowStatusBadRequest(err error, c *gin.Context) {
+	c.JSON(http.StatusBadRequest, gin.H{
+		"status":  http.StatusBadRequest,
+		"error": err.Error(),
+	})
+}
+
+func ThrowStatusUnauthorized(c *gin.Context) {
+	c.JSON(http.StatusUnauthorized, gin.H{
+		"status":  http.StatusUnauthorized,
+		"error": ErrUnauthorized,
+	})
+}
